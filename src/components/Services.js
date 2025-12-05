975fd0e43ec7590472308189d0b6b514062a2ee3
minahal-at-insane <98262211+minahal-at-insane@users.noreply.github.com>
2025-12-05 14:23:33 +0500

    add services images

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,13 +1,21 @@
 import React, { useEffect } from 'react'
-import benefit1 from '../assets/benefit1.webp';
-import benefit2 from '../assets/benefit2.webp';
-import benefit3 from '../assets/benefit3.webp';
-import benefit4 from '../assets/benefit4.webp';
-import benefit5 from '../assets/benefit5.webp';
-import benefit6 from '../assets/benefit6.webp';
-import benefit7 from '../assets/benefit7.webp';
-import benefit8 from '../assets/benefit8.webp';
-import benefit9 from '../assets/benefit9.webp';
+import service1 from '../assets/service/service1.png';
+import service2 from '../assets/service/service2.png';
+import service3 from '../assets/service/service3.png';
+import service4 from '../assets/service/service4.png';
+import service5 from '../assets/service/service5.png';
+import service6 from '../assets/service/service6.png';
+import service7 from '../assets/service/service7.png';
+import service8 from '../assets/service/service8.png';
+import service9 from '../assets/service/service9.png';
+import service10 from '../assets/service/service10.png';
+import service11 from '../assets/service/service11.png';
+import service12 from '../assets/service/service12.png';
+import service13 from '../assets/service/service13.png';
+import service14 from '../assets/service/service14.png';
+import service15 from '../assets/service/service15.png';
+
+
 import Menu from './Menu'
 import AOS from 'aos';
 import 'aos/dist/aos.css';
@@ -15,50 +23,85 @@ import 'aos/dist/aos.css';
 
 function Services() {
 
+      useEffect(() => {
+        AOS.init({
+          duration: 600,
+        });
+      }, [])
 
     const Services = [
         {
-            img: benefit1,
+            img: service1,
             title: "Premium quality",
             desc: "Professional, high-quality social media content that get results.",
         },
         {
-            img: benefit2,
+            img: service2,
             title: "Super-fast delivery",
             desc: "Receive your first batch of content within 5 business days.",
         },
         {
-            img: benefit3,
+            img: service3,
             title: "Fixed monthly rate",
             desc: "One flat fee with no surprises or hidden costs each month.",
         },
         {
-            img: benefit4,
+            img: service2,
             title: "Unlimited revisions",
             desc: "Request as many tweaks as you need until you are satisfied.",
         },
         {
-            img: benefit5,
+            img: service5,
             title: "Flexible & scalable",
             desc: "Easily upgrade, downgrade or cancel your plan anytime.",
         },
         {
-            img: benefit6,
+            img: service6,
             title: "Team collaboration",
             desc: "Invite unlimited team members to suggest ideas and give feedback.",
         },
         {
-            img: benefit7,
+            img: service7,
             title: "Consistent branding",
             desc: "Premium content that matches your brand style and tone.",
         },
         {
-            img: benefit8,
+            img: service8,
             title: "Unique and yours",
             desc: "Every piece of content is 100% owned by you for you to reuse however you like.",
         },
         {
-            img: benefit9,
+            img: service9,
+            title: "Dedicated support",
+            desc: "Get expert email or video call support whenever you need it.",
+        },
+          {
+            img: service10,
+            title: "Dedicated support",
+            desc: "Get expert email or video call support whenever you need it.",
+        },
+          {
+            img: service11,
+            title: "Dedicated support",
+            desc: "Get expert email or video call support whenever you need it.",
+        },
+          {
+            img: service12,
+            title: "Dedicated support",
+            desc: "Get expert email or video call support whenever you need it.",
+        },
+          {
+            img: service13,
+            title: "Dedicated support",
+            desc: "Get expert email or video call support whenever you need it.",
+        },
+          {
+            img: service14,
+            title: "Dedicated support",
+            desc: "Get expert email or video call support whenever you need it.",
+        },
+          {
+            img: service15,
             title: "Dedicated support",
             desc: "Get expert email or video call support whenever you need it.",
         },
